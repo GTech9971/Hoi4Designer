58b100947b5325d2dacb9e31f0fb2e08f0135838
George <georgetech1984@gmail.com>
2025-06-04 11:59:58 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AircraftDesigner from './components/AircraftDesigner';
+
+function App() {
+  return (
+    <div className="App">
+      <AircraftDesigner />
+    </div>
+  );
+}
+
+export default App;
